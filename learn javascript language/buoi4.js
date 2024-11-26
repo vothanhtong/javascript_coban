6e48ccd5fdf340b9e5ba51dc387f25f9d6df351f
vothanhtong <vothanhtong9295@gmail.com>
2024-11-26 10:38:02 +0700

    Toán tử logic

--- learn javascript language/buoi4.js
+++ learn javascript language/buoi4.js
@@ -17,4 +17,66 @@
 // - Kết hợp nhiều điều kiện trong câu lệnh `if-else`.  
 // - Điều chỉnh logic của các vòng lặp hoặc biểu thức.  
 
-#
+//  Ví dụ 1: Câu lệnh điều kiện
+
+let xx = 8;
+let yx = 3;
+if ((xx > 5) && (yy < 10)) {
+    console.log("Điều kiện đúng!");
+}
+
+//  Ví dụ 2: Sử dụng `!`
+
+let z = 4;
+if (!(z > 5)) {
+    console.log("z không lớn hơn 5");
+}
+
+// Ví dụ 3: Kết hợp `&&` và `||`
+
+let x = 6;
+let y = 15;
+if ((x > 5) || (y < 10) && (x < 10)) {
+    console.log("Đúng!");
+}
+
+
+//  4. Thứ tự ưu tiên của toán tử logic
+// 1. **`!`** (Phủ định - ưu tiên cao nhất)  
+// 2. **`&&`** (Và)  
+// 3. **`||`** (Hoặc - ưu tiên thấp nhất)  
+
+// > **Lưu ý**: Dấu ngoặc `()` nên được sử dụng để đảm bảo thứ tự ưu tiên đúng theo ý muốn.  
+
+// Thực hành
+
+// Dưới đây là một số bài tập:  
+
+// 1. Kiểm tra số nằm trong khoảng
+// Viết chương trình kiểm tra xem một số `n` có nằm trong khoảng từ 10 đến 20 không:  
+
+let n = 15;
+if (n >= 10 && n <= 20) {
+    console.log("Số nằm trong khoảng 10 đến 20");
+} else {
+    console.log("Số không nằm trong khoảng");
+}
+
+// 2.Số chẵn hoặc số âm
+// Viết chương trình kiểm tra xem một số có phải là số chẵn hoặc số âm không:  
+
+let nn = -4;
+if (n % 2 === 0 || n < 0) {
+    console.log("Số là số chẵn hoặc số âm");
+} else {
+    console.log("Số không phải số chẵn và cũng không phải số âm");
+}
+// 3. Sử dụng `!` để kiểm tra điều kiện ngược lại
+// Kiểm tra nếu một số không nằm trong khoảng từ 1 đến 100:  
+
+let nx = 150;
+if (!(n >= 1 && n <= 100)) {
+    console.log("Số không nằm trong khoảng 1 đến 100");
+} else {
+    console.log("Số nằm trong khoảng 1 đến 100");
+}
